1c2449169d7cbe2820b1df546b43a5cdbf2fadb7
Balajanovski <jbalajanbusiness@gmail.com>
2022-03-26 14:26:36 +1100

    - Book keeping

--- book_keeping/bid_and_ask.py
+++ book_keeping/bid_and_ask.py
@@ -22,8 +22,6 @@ class BidAndAsk:
         return self._asks[instrument]
 
     def account_for_trade(self, instrument: str, order: Order):
-        return
-
         if len(self._bids[instrument]) == 0 or self._bids[instrument][-1].price < order.price:
             target = self._asks
         else:
